7904f884b2ffb27b3ba3e0ed7d0e31b630a06410
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-28 18:50:57 +0300

    Try out some code with print() method

--- Debugging/ViewController.swift
+++ Debugging/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("I'm inside the viewDidLoad() method!")
+        print(1, 2, 3, 4, 5)
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: "")
     }
 
 
